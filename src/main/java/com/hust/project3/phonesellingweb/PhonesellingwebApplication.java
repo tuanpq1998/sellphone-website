df944df1035506808a35db2e040ef9639d1c4865
tuanpq1998 <tuanquang123321@gmail.com>
2021-06-07 23:04:11 +0700

    test gitignore

--- src/main/java/com/hust/project3/phonesellingweb/PhonesellingwebApplication.java
+++ src/main/java/com/hust/project3/phonesellingweb/PhonesellingwebApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication 
-public class PhonesellingwebApplication {
+public class PhonesellingwebApplication { 
 
 	public static void main(String[] args) {
 		SpringApplication.run(PhonesellingwebApplication.class, args);
