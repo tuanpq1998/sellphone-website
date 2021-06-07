c472e7527c840779b9e47609ea87152c7a6aff3c
tuanpq1998 <tuanquang123321@gmail.com>
2021-06-07 23:02:43 +0700

    update gitignore

--- src/main/java/com/hust/project3/phonesellingweb/PhonesellingwebApplication.java
+++ src/main/java/com/hust/project3/phonesellingweb/PhonesellingwebApplication.java
@@ -3,7 +3,7 @@ package com.hust.project3.phonesellingweb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication 
 public class PhonesellingwebApplication {
 
 	public static void main(String[] args) {
